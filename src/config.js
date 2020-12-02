123f0bef59cdec6775b8eed7072488ec836ca3de
Tom Morgan <thomas.morgan1903@gmail.com>
2020-12-02 22:06:46 +0000

    added config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const API_KEY = 'apikey=thewdb';
+export const API_URL =
+  'https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/';
+
+export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIE = 'GET_MOVIE';
+export const LOAD_MOVIES = 'LOAD_MOVIES';
+export const LOAD_MOVIE = 'LOAD_MOVIE';
+export const MOVIES_ERROR = 'MOVIES_ERROR';
+export const MOVIE_ERROR = 'MOVIE_ERROR';
+export const CLEAR_MOVIE = 'CLEAR_MOVIE';
