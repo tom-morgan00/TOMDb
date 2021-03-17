45135400664995827daf7ff28cb3882ecc82c7d4
Thomas Morgan <70469977+tom-morgan00@users.noreply.github.com>
2021-03-17 19:41:06 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_KEY = 'apikey=1c94777f';
 export const API_URL =
-  'https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/';
+  'http://www.omdbapi.com/';
 
 export const GET_MOVIES = 'GET_MOVIES';
 export const GET_MOVIE = 'GET_MOVIE';
